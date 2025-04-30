3e265888de640d1ae647b845c062b8a88b1a9a8f
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2025-04-30 21:49:18 +0530

    added files

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const sessionOptions = {
     },
 };
 
-app.engine("ejs", ejsMate);
+app.engine("ejs", );
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "/public")));
